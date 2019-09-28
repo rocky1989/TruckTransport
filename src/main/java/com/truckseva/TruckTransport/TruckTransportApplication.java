62b0db5a3ee76a576734d1992163b7315e6e7c3c
kumar <kumar@ROCKY-PC>
2019-09-28 23:03:19 +0530

    initial setup

---
+++ src/main/java/com/truckseva/TruckTransport/TruckTransportApplication.java
@@ -0,0 +1,13 @@
+package com.truckseva.TruckTransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckTransportApplication.class, args);
+	}
+
+}
